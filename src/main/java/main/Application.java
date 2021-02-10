b71e26da9f18cc801fa90b1ee38f1111793ea313
Aneirine <anuashi2002@gmail.com>
2021-02-10 17:56:22 +0200

    Configure DB

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,7 +1,11 @@
 package main;
 
+import com.google.gson.Gson;
+import main.routes.UserRoute;
+
 public class Application {
     public static void main(String[] args) {
-
+        Gson gson = new Gson();
+        UserRoute.configureUserRoutes(gson);
     }
 }
