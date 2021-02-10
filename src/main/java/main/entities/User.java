45b409393634ac5b7a392201c7fabad33324efce
Aneirine <anuashi2002@gmail.com>
2021-02-10 16:30:30 +0200

    Add user entity

---
+++ src/main/java/main/entities/User.java
@@ -0,0 +1,21 @@
+package main.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
