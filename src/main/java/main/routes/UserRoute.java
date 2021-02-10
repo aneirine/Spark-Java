c9b8e08cebdae20a3b1517bbe22cd9bf46d4e094
Aneirine <anuashi2002@gmail.com>
2021-02-10 17:44:41 +0200

    Add User rpute and service

---
+++ src/main/java/main/routes/UserRoute.java
@@ -0,0 +1,6 @@
+package main.routes;
+
+public class UserRoute {
+
+    private static UserService userService;
+}
---
+++ src/main/java/main/services/UserService.java
@@ -0,0 +1,26 @@
+package main.services;
+
+import com.google.gson.Gson;
+import main.entities.User;
+import main.repositories.Repository;
+import main.repositories.UserRepository;
+import spark.Request;
+
+import java.util.List;
+
+public class UserService {
+    private Repository<Long, User> repository;
+
+    public UserService() {
+        this.repository = new UserRepository();
+    }
+
+    public List<User> findAllUsers(){
+        return repository.findAll();
+    }
+
+    public User save(Request request, Gson gson) {
+        User user = gson.fromJson(request.body(), User.class);
+        return repository.save(user);
+    }
+}
