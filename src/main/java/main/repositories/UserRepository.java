112b2dcf604e1853ec0a324d04b85fa1d40b9c1c
Aneirine <anuashi2002@gmail.com>
2021-02-10 17:40:01 +0200

    Add user repository methods

--- src/main/java/main/repositories/UserRepository.java
+++ src/main/java/main/repositories/UserRepository.java
@@ -1,13 +1,57 @@
 package main.repositories;
 
-import org.eclipse.jetty.websocket.common.SessionFactory;
 
-public class UserRepository {
+import main.entities.User;
+import main.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class UserRepository implements Repository<Long ,User>{
 
     private SessionFactory sessionFactory;
 
     public UserRepository() {
-
+        this.sessionFactory = HibernateUtils.getSessionFactory();
     }
 
+
+    public User save(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.saveOrUpdate(user);
+            session.flush();
+            session.close();
+            return user;
+        }
+    }
+
+    public void delete(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.flush();
+            session.close();
+        }
+    }
+
+    public List<User> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            TypedQuery<User> query = session.createQuery("from User", User.class);
+            List<User> users = query.getResultList();
+            session.close();
+            return users;
+        }
+    }
+
+    public User findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.get(User.class, id);
+            session.close();
+            return user;
+        }
+    }
 }
--- src/main/java/main/utils/HibernateUtils.java
+++ src/main/java/main/utils/HibernateUtils.java
@@ -3,16 +3,19 @@ package main.utils;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class UtilHibernate {
+public class HibernateUtils {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
             return new Configuration().configure().buildSessionFactory();
-        }
-        catch (Throwable e) {
+        } catch (Throwable e) {
             e.printStackTrace();
             throw new ExceptionInInitializerError(e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
